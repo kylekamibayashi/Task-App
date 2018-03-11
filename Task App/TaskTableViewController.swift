345aa8f3f625cf1b01c0be644bfba245c660c725
Kyle Kamibayashi <kylekamibayashi@claires-macbook-pro.local>
2018-03-11 15:41:58 -0700

    Added TaskTableViewController that added an array of tasks to be displayed.

---
+++ Task App/TaskTableViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class TaskTableViewController: UITableViewController {
+    
+    var tasks: [String] = ["Task1", "Task2", "Task3"]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let task = tasks[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tasksCell")!
+        cell.textLabel?.text = task
+        return cell
+    }
+}
