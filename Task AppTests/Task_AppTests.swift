dee071e1ecce1c987d1a9eea6d3291079b852bd8
Kyle Kamibayashi <kylekamibayashi@claires-macbook-pro.local>
2018-03-11 15:12:35 -0700

    Initial Commit

---
+++ Task AppTests/Task_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Task_AppTests.swift
+//  Task AppTests
+//
+//  Created by Kyle Kamibayashi on 3/11/18.
+//  Copyright © 2018 Kyle Kamibayashi. All rights reserved.
+//
+
+import XCTest
+@testable import Task_App
+
+class Task_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
